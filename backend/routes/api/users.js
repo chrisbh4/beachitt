0bead4b192a5ecdf5902be8a7cb2d34d3e3e4256
Chrisbh4 <chrismbh4@gmail.com>
2021-07-19 15:47:36 -0700

    option 1 or 2 for api/users.js

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
+// option 1
+// const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { setTokenCookie} = require('../../utils/auth');
 const { User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
@@ -30,9 +31,8 @@ const validateSignup = [
 
 
 // Sign up
-router.post(
-  '/',
-  validateSignup,
+router.post( '/',
+ validateSignup,
   asyncHandler(async (req, res) => {
     const { email, password, username } = req.body;
     const user = await User.signup({ email, username, password });
