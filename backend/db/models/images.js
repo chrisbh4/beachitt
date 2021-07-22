c2c4313f4cfa21b320e634b0a26297f47299ad74
Chrisbh4 <chrismbh4@gmail.com>
2021-07-21 20:21:42 -0700

    fixed Images db to be TEXT

--- backend/db/models/images.js
+++ backend/db/models/images.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Images = sequelize.define('Images', {
     rentalUnitId: DataTypes.INTEGER,
-    url: DataTypes.STRING
+    url: DataTypes.TEXT
   }, {});
   Images.associate = function(models) {
     // associations can be defined here
