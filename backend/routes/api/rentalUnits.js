92eeb61d1489cf67ff8f14460ad3d4e2ecb5f0c3
Chrisbh4 <chrismbh4@gmail.com>
2021-07-22 19:49:28 -0700

    All backend routes working

--- backend/routes/api/rentalUnits.js
+++ backend/routes/api/rentalUnits.js
@@ -1,30 +1,69 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const {RentalUnits} = require('../../db/models')
-const {requireAuth , setTokenCookie} = require('../../utils/auth')
+const { RentalUnits } = require('../../db/models')
+const { requireAuth, setTokenCookie } = require('../../utils/auth')
 // import { csrfProtection } from '../../utils/utils';
 // const newUnitValidation = require('../../utils/validation')
 
 
-const router= express.Router();
+const router = express.Router();
 //* requireAuth: middleware that is used to make sure only logged in users can hit certain routes
 
 
-router.get('/' , asyncHandler(async( _req , res )=>{
-    const allRentalUnits = await RentalUnits.findAll()
-      return res.json(allRentalUnits)
+router.get('/', asyncHandler(async (_req, res) => {
+  const allRentalUnits = await RentalUnits.findAll()
+  return res.json(allRentalUnits)
 }));
 
 
-router.post('/new', requireAuth , asyncHandler( async ( req ,res )=>{
-  const {title, ownerId ,  city,  distanceFromBeach, lat, lng,
-    pool, price , rentalUnitDescription, bathrooms, unitType , rooms, state, zipcode,totalRentals } = req.body
+router.get('/:id', asyncHandler(async (req, res) => {
+  const unit = await RentalUnits.findByPk(req.params.id)
+  res.json({ unit })
 
-    const newUnit = await RentalUnits.create({title,ownerId,  city,  distanceFromBeach, lat, lng,  pool, price ,
-      rentalUnitDescription, bathrooms , unitType , rooms , state, zipcode , totalRentals});
+}))
 
-    return res.json({  newUnit  });
-  }))
+router.post('/new', requireAuth, asyncHandler(async (req, res) => {
+  const { title, ownerId, city, distanceFromBeach, lat, lng,
+    pool, price, rentalUnitDescription, bathrooms, unitType, rooms, state, zipcode, totalRentals } = req.body
 
+  const newUnit = await RentalUnits.create({
+    title, ownerId, city, distanceFromBeach, lat, lng, pool, price,
+    rentalUnitDescription, bathrooms, unitType, rooms, state, zipcode, totalRentals
+  });
+
+  return res.json({ newUnit });
+}))
+
+router.put('/:id', requireAuth, asyncHandler(async( req, res )=>{
+  const unit = await RentalUnits.findByPk(req.params.id);
+      unit.title = req.body.title;
+      unit.city = req.body.city;
+      unit.distanceFromBeach = req.body.distanceFromBeach;
+      unit.lat = req.body.lat;
+      unit.lng = req.body.lng;
+      unit.pool = req.body.pool;
+      unit.price = req.body.price;
+      unit.rentalUnitDescription = req.body.rentalUnitDescription;
+      unit.bathrooms = req.body.bathrooms;
+      unit.unitType = req.body.unitType;
+      unit.rooms = req.body.rooms;
+      unit.state = req.body.state;
+      unit.zipcode = req.body.zipcode;
+
+  await unit.save()
+  return res.json({unit})
+
+}))
+
+
+
+router.delete('/:id', requireAuth, asyncHandler(async (req, res) => {
+  const rentalUnit = await RentalUnits.findByPk(req.params.id);
+
+  if (!rentalUnit) new Error(' Cannot find Rental Unit ');
+
+  await rentalUnit.destroy()
+  return
+}))
 
 module.exports = router;
