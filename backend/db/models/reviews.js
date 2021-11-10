94a94d901f38aa30364ed506aa01e6d2476b7cb6
Chrisbh4 <chrismbh4@gmail.com>
2021-11-09 16:53:43 -0800

    reviews backend complete

--- backend/db/models/reviews.js
+++ backend/db/models/reviews.js
@@ -2,14 +2,14 @@
 module.exports = (sequelize, DataTypes) => {
   const Reviews = sequelize.define('Reviews', {
     comment: DataTypes.STRING,
-    rating: DataTypes.INTEGER,
+    // rating: DataTypes.INTEGER,
     rentalUnitId: DataTypes.INTEGER,
     userId: DataTypes.INTEGER
   }, {});
   Reviews.associate = function(models) {
     // associations can be defined here
     Reviews.belongsTo(models.User, {foreignKey:'userId'})
-    Reviews.belongsTo(models.RentalUnits, {foreignKey:'rentalUnitsId'})
+    Reviews.belongsTo(models.RentalUnits, {foreignKey:'rentalUnitId'})
   };
   return Reviews;
 };
--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,14 +1,55 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const Reviews = require('../../db/models')
+const {Reviews} = require('../../db/models')
+const {dataAdjuster} = require('../../utils/utils')
 
 const router = express.Router();
 
 
-// router.post('/new', asyncHandler( async ( req , res )=>{
-//      const review =
+router.get('/', asyncHandler( async ( req , res )=>{
 
-//      res.json({})
-// }))
 
-module.exports = router ;
+    const data = await Reviews.findAll();
+    const reviews = dataAdjuster(data);
+
+    return res.json({reviews})
+
+}))
+
+router.get('/:id', asyncHandler( async ( req , res )=>{
+
+    const review = await Reviews.findByPk(req.params.id);
+    return res.json({review})
+    // return res.json(review})
+}))
+
+
+router.post('/new', asyncHandler( async ( req , res )=>{
+     const {comment, rentalUnitId, userId}= req.body;
+
+     const review = await Reviews.create({comment,rentalUnitId,userId});
+
+    return res.json({review})
+}))
+
+router.put('/:id', asyncHandler( async ( req , res )=>{
+
+    const review = await Reviews.findByPk(req.params.id);
+    const {comment}= req.body;
+
+    review.comment = comment;
+    await review.save();
+
+    return res.json({review})
+}))
+
+router.delete('/:id', asyncHandler( async ( req , res )=>{
+
+    const review = await Reviews.findByPk(req.params.id);
+    await review.destroy();
+
+    return res.send("review has been deleted")
+
+}))
+
+module.exports = router;
