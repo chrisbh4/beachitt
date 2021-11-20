a68d1d42dd806618f07203defd86827c95458935
Chrisbh4 <chrismbh4@gmail.com>
2021-11-19 18:54:50 -0800

    quick break

--- frontend/src/components/RentalUnitsPage/UnitPage/index.js
+++ frontend/src/components/RentalUnitsPage/UnitPage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getRentalUnits, getSingleUnit } from '../../../store/rentalUnits';
