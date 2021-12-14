ced2908e771252c6efff1f05907184edd8ba0881
Chrisbh4 <chrismbh4@gmail.com>
2021-12-13 18:32:15 -0800

    added notes for tomorrow

--- frontend/src/components/Booking-Cal/index.js
+++ frontend/src/components/Booking-Cal/index.js
@@ -4,8 +4,9 @@ import 'react-calendar/dist/Calendar.css';
 
 
 /* Progress Table
-
         * Need to be able to remove the empty Object that is attached to the string of the selected Date
+Inside the console the selected dates are being shown but with an empty Object I need to be able to remove that empty object at the end of both dates and be able to hold the selected dates as a
+string so I can upload the splitted strings as seperated data parts that will be uploaded as day,month,year for both startDate and endDate
 
 */
 
