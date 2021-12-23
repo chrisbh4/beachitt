2da78e8ac1c776e13b9dfa4cf5b2f4911769bad4
Chrisbh4 <chrismbh4@gmail.com>
2021-12-22 21:59:18 -0800

    reviewValiations created

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const {Reviews} = require('../../db/models')
-const {dataAdjuster} = require('../../utils/utils')
+const {Reviews} = require('../../db/models');
+const {dataAdjuster} = require('../../utils/utils');
+const {handleValidationErrors} = require('../../utils/validation');
+const {check} = require("express-validator");
 
 const router = express.Router();
 
+const reviewValiation =[
+    check("comment")
+    .isLength({min:5,max:100})
+    .withMessage("Review must be bettwen 5-100 characters."),
+    handleValidationErrors
+]
 
 router.get('/', asyncHandler( async ( req , res )=>{
 
@@ -24,14 +32,14 @@ router.get('/:id', asyncHandler( async ( req , res )=>{
 }))
 
 
-router.post('/new', asyncHandler( async ( req , res )=>{
+router.post('/new',reviewValiation, asyncHandler( async ( req , res )=>{
      const {comment, rentalUnitId, userId,username}= req.body;
      const review = await Reviews.create({comment,rentalUnitId,userId,username});
 
     return res.json({review})
 }))
 
-router.put('/:id', asyncHandler( async ( req , res )=>{
+router.put('/:id', reviewValiation, asyncHandler( async ( req , res )=>{
 
     const review = await Reviews.findByPk(req.params.id);
     const {comment}= req.body;
