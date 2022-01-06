76b0af7e0210601f3a8f40bc9ac5dee533d34652
Chrisbh4 <chrismbh4@gmail.com>
2022-01-06 07:55:39 -0800

    Re organized the units store file

--- frontend/src/store/rentalUnits.js
+++ frontend/src/store/rentalUnits.js
@@ -25,106 +25,109 @@ const load = units => ({
     unitId,
   })
 
-export const editRentalUnit = (payload ,unitId)=> async dispatch =>{
-
-  const formData = new FormData();
-  const {title,city,state,zipcode,distanceFromBeach,rooms,bathrooms,pool,unitType,lat,lng, price, rentalUnitDescription ,url } = payload
-  formData.append("title",title)
-  formData.append("city",city)
-  formData.append("state",state)
-  formData.append("zipcode",zipcode)
-  formData.append("distanceFromBeach",distanceFromBeach)
-  formData.append("rooms",rooms)
-  formData.append("bathrooms",bathrooms)
-  formData.append("pool", pool)
-  formData.append("unitType",unitType)
-  formData.append("lat", lat)
-  formData.append("lng",lng)
-  formData.append("price",price)
-  formData.append("rentalUnitDescription", rentalUnitDescription)
 
 
-  if (url) formData.append("url",url);
-
-  const res = await csrfFetch(`/api/units/edit/${unitId}`,{
-    method: "PUT",
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-    body: formData,
-  });
-
-  const data = await res.json();
-
-  if(res.ok) dispatch(editUnit(data))
-  return data
-
-}
-
-export const deleteRentalUnit = (unitId)=> async dispatch=>{
-  const res = await csrfFetch(`/api/units/edit/${unitId}`,{
-    method:"DELETE"
-  });
-
-  const data = await res.json();
-
-  if(res.ok) dispatch(deleteUnit(data))
-  return data
-
-}
 
 
-//* grabs all units
-export const getRentalUnits = () => async dispatch => {
+  //* grabs all units
+  export const getRentalUnits = () => async dispatch => {
     const res = await csrfFetch(`/api/units`);
     const data = await res.json();
 
-      dispatch(load(data))
+    dispatch(load(data))
   };
 
-export const getSingleUnit = (unitId)=> async dispatch =>{
-  const rentalUnit = await csrfFetch(`/api/units/${unitId}`);
+  export const getSingleUnit = (unitId)=> async dispatch =>{
+    const rentalUnit = await csrfFetch(`/api/units/${unitId}`);
     const unit = await rentalUnit.json();
 
     dispatch(load(unit))
     return unit
-}
+  }
 
-export const createRentalUnit = (payload) => async dispatch =>{
-  const formData = new FormData();
-  const {title,ownerId,city,state,zipcode,distanceFromBeach,rooms,bathrooms,pool,unitType,lat,lng, price, rentalUnitDescription ,totalRental ,url } = payload
-  formData.append("title",title)
-  formData.append("ownerId",ownerId)
-  formData.append("city",city)
-  formData.append("state",state)
-  formData.append("zipcode",zipcode)
-  formData.append("distanceFromBeach",distanceFromBeach)
-  formData.append("rooms",rooms)
-  formData.append("bathrooms",bathrooms)
-  formData.append("pool", pool)
-  formData.append("unitType",unitType)
-  formData.append("lat", lat)
-  formData.append("lng",lng)
-  formData.append("price",price)
-  formData.append("rentalUnitDescription", rentalUnitDescription)
-  formData.append("totalRentals", totalRental)
 
-  if (url) formData.append("url",url);
+  export const createRentalUnit = (payload) => async dispatch =>{
+    const formData = new FormData();
+    const {title,ownerId,city,state,zipcode,distanceFromBeach,rooms,bathrooms,pool,unitType,lat,lng, price, rentalUnitDescription ,totalRental ,url } = payload
+    formData.append("title",title)
+    formData.append("ownerId",ownerId)
+    formData.append("city",city)
+    formData.append("state",state)
+    formData.append("zipcode",zipcode)
+    formData.append("distanceFromBeach",distanceFromBeach)
+    formData.append("rooms",rooms)
+    formData.append("bathrooms",bathrooms)
+    formData.append("pool", pool)
+    formData.append("unitType",unitType)
+    formData.append("lat", lat)
+    formData.append("lng",lng)
+    formData.append("price",price)
+    formData.append("rentalUnitDescription", rentalUnitDescription)
+    formData.append("totalRentals", totalRental)
 
-  const res = await csrfFetch(`/api/units/new`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-    body: formData,
-  });
+    if (url) formData.append("url",url);
 
-  const data = await res.json();
+    const res = await csrfFetch(`/api/units/new`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      body: formData,
+    });
 
-  if(res.ok) dispatch(addUnit(data))
+    const data = await res.json();
+    if(res.ok) dispatch(addUnit(data))
 
-  return data
-}
+    return data
+  }
+
+
+  export const editRentalUnit = (payload ,unitId)=> async dispatch =>{
+
+    const formData = new FormData();
+    const {title,city,state,zipcode,distanceFromBeach,rooms,bathrooms,pool,unitType,lat,lng, price, rentalUnitDescription ,url } = payload
+    formData.append("title",title)
+    formData.append("city",city)
+    formData.append("state",state)
+    formData.append("zipcode",zipcode)
+    formData.append("distanceFromBeach",distanceFromBeach)
+    formData.append("rooms",rooms)
+    formData.append("bathrooms",bathrooms)
+    formData.append("pool", pool)
+    formData.append("unitType",unitType)
+    formData.append("lat", lat)
+    formData.append("lng",lng)
+    formData.append("price",price)
+    formData.append("rentalUnitDescription", rentalUnitDescription)
+
+    if (url) formData.append("url",url);
+
+    const res = await csrfFetch(`/api/units/edit/${unitId}`,{
+      method: "PUT",
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      body: formData,
+    });
+
+    const data = await res.json();
+
+    if(res.ok) dispatch(editUnit(data))
+    return data
+
+  }
+
+
+export const deleteRentalUnit = (unitId)=> async dispatch=>{
+    const res = await csrfFetch(`/api/units/edit/${unitId}`,{
+      method:"DELETE"
+    });
+
+    const data = await res.json();
+
+    if(res.ok) dispatch(deleteUnit(data))
+    return data
+  }
 
 
 /*
