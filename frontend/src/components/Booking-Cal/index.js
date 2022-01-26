08bb10e9de3d3c2e25fb4702456488442bd45590
Chrisbh4 <chrismbh4@gmail.com>
2022-01-25 18:33:50 -0800

    Debugging errors and setErrors scoping

--- frontend/src/components/Booking-Cal/index.js
+++ frontend/src/components/Booking-Cal/index.js
@@ -52,176 +52,74 @@ function BookingCal({ userId, unitId, unitBookings }) {
     }
 
     function isBookingOpen(unitStart, unitEnd, checkStart, checkEnd) {
-    //* can delete if not being used
-        // const splitStart = unitStart.split("-");
-        // const splitEnd = unitEnd.split("-");
-        // const checkStartSplit = checkStart.split("-");
-        // const checkEndSplit = checkEnd.split("-");
-
-        // const unitStartDate = new Date(splitStart[2], parseInt(splitStart[1]) - 1, splitStart[0]);
-        // const testDate = Date.parse(unitStart)
-        // console.log("Date test:", test)
-        //* I  can place the unit.booking data inside the conditional as is since the func renders it
-        //* need to verify by checking logs
-        // console.log(Date.parse(unitStart) < Date.parse(checkStart))
-        // console.log("unit", Date.parse(unitStart))
-        // console.log("check", Date.parse(checkStart))
-
-
-        // console.log(2 > 1)
-        // const unitEndDate = new Date(splitEnd[2], parseInt(splitEnd[1]) - 1, splitEnd[0]);
-        // const bookingStartDate = new Date(checkStartSplit[2], parseInt(checkStartSplit[1]) - 1, checkStartSplit[0]);
-        // const bookingEndDate = new Date(checkEndSplit[2], parseInt(checkEndSplit[1]) - 1, checkEndSplit[0]);
-
-
         const unitStartDate = Date.parse(unitStart)
         const unitEndDate = Date.parse(unitEnd)
         const bookingStartDate = Date.parse(checkStart)
         const bookingEndDate = Date.parse(checkEnd)
 
-//* original
-        // if (bookingStartDate > unitStartDate && bookingStartDate < unitEndDate) {
-             // if startDate is in-between unit.startDate and unit.endDate return true
-            // if startDate is not in-between unit.start unit.end : then return false
-         // if ((bookingStartDate > unitStartDate && bookingStartDate < unitEndDate) || (bookingEndDate > unitStartDate && bookingEndDate < unitEndDate)) {
-        //    //* true = not available
-        //     console.log(true)
-        //     return true
-        // }
-        // else {
-        //     //* if else then it means its open
-        //     console.log("IS open :", false)
-        //     return false
-        // }
 
-
-//* test
-//* need to remeber to set the errors state back to an empty array
+        //* need to remeber to set the errors state back to an empty array
         if ((bookingStartDate > unitStartDate && bookingStartDate < unitEndDate) || (bookingEndDate > unitStartDate && bookingEndDate < unitEndDate)) {
-        // if (bookingStartDate > unitStartDate && bookingStartDate < unitEndDate) {
-        // if (bookingStartDate > unitStartDate) {
-
             //* true = not available
-            console.log("greater than unit StartDate // not available")
-            console.log("less than unit EndDate // not available")
             return true
         }
 
-        // if ( bookingStartDate < unitEndDate){
-        //     // return true
-        // }
+        return false
+    };
 
 
-return false
+console.log("errors outside func :", errors)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        unitBookings?.forEach(async(booking) => {
+            const unitStartDate = booking.startDate;
+            const unitEndDate = booking.endDate;
+            const result = isBookingOpen(unitStartDate, unitEndDate, startDate, endDate);
+            //* This stops the forEach : only if the helper returns true
+            console.log("result :", result)
+            if (result === true) {
+               await setErrors(["booking is unavailable, check bookings list to see booked dates."])
+                return errors;
+            }
+        });
+
+        //setErrors is used but the errors.length never gets touched
+        //* booking errors are being set into errors from the forEach but it doesn't register for the if condition
+        if(errors){
+        // if(errors[0].length){
+            // return;
+            console.log("error has length :", errors);
+            return;
+        }else{
+            const payload = { startDate, endDate, userId, rentalUnitId: unitId }
+            const data = await dispatch(fetchAddBooking(payload))
+
+
+            if (data.errors) {
+                setErrors(data.errors)
+                return data.errors
+            }
+            await dispatch(getSingleUnit(unitId))
+            return data
+
+        }
+
+
     };
 
 
 
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        unitBookings?.forEach((booking) => {
-            const unitStartDate = booking.startDate;
-            const unitEndDate = booking.endDate;
-            const result = isBookingOpen(unitStartDate, unitEndDate, startDate, endDate);
-//* This stops the forEach : only if the helper returns true
-console.log("result :", result)
-            if (result === true) {
-                setErrors(["booking is unavailable, check bookings list to see booked dates."])
-                return {msg:"can not double book"}
-            }
-            // else{
-            //     setErrors([])
-            // }
-        })
-
-
-//* if errors.length then dont have the code below run just return the func else dispatch
-        const payload = { startDate, endDate, userId, rentalUnitId: unitId }
-        const data = await dispatch(fetchAddBooking(payload))
-
-
-        if (data.errors) {
-            setErrors(data.errors)
-            return data.errors
-        }
-        dispatch(getSingleUnit(unitId))
-        return data
-
-        // const payload = {startDate, endDate ,userId, rentalUnitId:unitId}
-        // const data = await dispatch(fetchAddBooking(payload))
-
-        // if(data.errors){
-            //     setErrors(data.errors)
-            //     return data.errors
-            // }
-            // dispatch(getSingleUnit(unitId))
-
-        };
-
-
-    //* new Date() converts it to date format
-
-    // console.log(new Date(startDate))
-    // console.log(new Date(endDate))
-
-
     /*
-    * Notes for configuring the function below
-    - I'm able to access all the bookings that belong to the unit
-        - Need to iterate through the bookings and grab both startDate and endDate
-        - each iteration will throw both [ startDate, endDate ] inside the helper function that does the date checking
-        Helper Function
-        * will need to have an array that holds the error values that are pushed into it then that array will then be placed inside the setErrors function
-        - index into the argument array and create two variables startCheck, endCheck : this will represent the selected dates
-        (conditional rendering)
-            - if startDate returns an error then have that error be pushed into the errorsArray : have the function return to stop the func from continuting
-            - if endDate returns any errors then have them pushed into the errors Array
-            - else dispatch the new booking
-
              * Bookings validate frontend funitonality
             Link: https://www.geeksforgeeks.org/how-to-check-if-one-date-is-between-two-dates-in-javascript/
 
     */
 
-    const validateBookingDates = () => {
-        unitBookings?.forEach((booking) => {
-            const unitStartDate = booking.startDate;
-            const unitEndDate = booking.endDate;
 
-            const result = isBookingOpen(unitStartDate, unitEndDate, startDate, endDate);
 
-            if (result === true) {
-                setErrors("booking is unavailable, check bookings list to see booked dates.")
-            };
-        })
-
-    }
-    //iterate through this allows me to access the units.data
-    // place the itearted data and the selectedDates inside the helper function
-    // have to check both the selectedStart and selectedEnd inside the helper function
-    // if the helper function returns false or true then that gives me my answer
-
-    // function isBookingOpen(unitStart, unitEnd, checkStart, checkEnd) {
-    //    const splitStart = unitStart.split("-");
-    //    const splitEnd = unitEnd.split("-");
-    //    const checkStartSplit = checkStart.split("-");
-    //    const checkEndSplit = checkEnd.split("-");
-
-    //     const unitStartDate = new Date(splitStart[2], parseInt(splitStart[1]) - 1, splitStart[0]);
-    //     const unitEndDate = new Date(splitEnd[2], parseInt(splitEnd[1]) - 1, splitEnd[0]);
-    //     const bookingStartDate = new Date(checkStartSplit[2], parseInt(checkStartSplit[1]) - 1, checkStartSplit[0]);
-    //     const bookingEndDate = new Date(checkEndSplit[2], parseInt(checkEndSplit[1]) - 1, checkEndSplit[0]);
-
-    //     if ( bookingStartDate > unitStartDate && bookingStartDate < unitEndDate || bookingEndDate > unitStartDate && bookingEndDate < unitEndDate ) {
-    //         //* true = the date is unavilable
-    //         //* false = the date is available
-    //        return true
-    //     } else {
-    //         return false
-    //     }
-    // };
 
 
 
