ee2b2cdfadc99bea01397f0768c5e9db08dbda8e
Chrisbh4 <chrismbh4@gmail.com>
2022-02-23 07:29:18 -0800

    code clean up

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRentalUnits } from '../../store/rentalUnits'
 import "../SplashPage/splashPage.css"
